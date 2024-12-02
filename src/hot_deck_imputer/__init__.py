7de744cfa0b6537ad8d270400a0bdababe0c48a2
Jamie Carter <jamesonarmstrongcarter@gmail.com>
2024-12-02 12:21:25 -0500

    update

--- src/hot_deck_imputer/__init__.py
+++ src/hot_deck_imputer/__init__.py
@@ -4,4 +4,4 @@ Intialization file necessary for the package to be recognized as a module.
 This can be empty.
 """
 from .hot_deck_class import HotDeckImputer
-import error_detection
+from error_detection import *
